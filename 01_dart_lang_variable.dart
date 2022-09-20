31cce0e9c3dcce0ba6217889a69aa0bbcb1475e1
zengcheng <1265207567@qq.com>
2022-09-20 19:38:04 +0800

    add test01

---
+++ 01_dart_lang_variable.dart
@@ -0,0 +1,45 @@
+void main(List<String> args) {
+  varaiable();
+}
+
+// If you use var to create a variable and don't give it an initial value,
+// it can be give 123 or "123".
+//
+// If you use Object or dynamic to create, the variable will be checked when
+// compile if you use Object and will not be checked when use dynamic.
+//
+// And if you use String, the variable will only give String.
+void varaiable() {
+  var name;
+  name = 123; // 123 is Int
+  print(name);
+  checkType(name);
+  name = "123"; // 123 is String
+  print(name);
+  checkType(name);
+  name = 123.123; // 123.123 is Double
+  print(name);
+  checkType(name);
+  // It will show "A value of type 'int' can't be assigned to a variable of type 'String'."
+  // var name_ini = "123";
+  // name_ini = 123;
+  dynamic name_dynamic = "Boy";
+  print(name_dynamic);
+  checkType(name_dynamic);
+  Object name_object = "Camie";
+  print(name_object);
+  checkType(name_object);
+  String name_string = "Dane";
+  print(name_string);
+  checkType(name_string);
+}
+
+void checkType(var value) {
+  if (value is String) {
+    print("${value} is String");
+  } else if (value is int) {
+    print("${value} is Int");
+  } else if (value is double) {
+    print("${value} is Double");
+  }
+}
