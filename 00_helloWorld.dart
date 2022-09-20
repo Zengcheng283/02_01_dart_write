6cd7f5207156e65bd23dd93eba53895f1cc23311
zengcheng <1265207567@qq.com>
2022-09-20 17:23:22 +0800

    push HelloWorld

---
+++ 00_helloWorld.dart
@@ -0,0 +1,13 @@
+// If you want to run the first simple programme, you can write code like this
+//
+// void main() {
+//   print("Hello World!");
+// }
+//
+//
+// And you can see ``Run|Debug`` at head of the main method,
+// click Run and the programme will run and the conslone will print ``"Hello World!"``
+
+void main() {
+  print("Hello World!");
+}
